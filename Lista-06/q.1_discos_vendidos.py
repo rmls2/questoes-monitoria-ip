3a6fecbe6ccdb2a5858c7651ea43d9d815434559
rmls2 <millerrobert182@gmail.com>
2024-04-17 22:13:08 -0300

    upload questions

---
+++ Lista-06/q.1_discos_vendidos.py
@@ -0,0 +1,31 @@
+
+discos_por_artistas = {'Priscila Senna': 40, 'Eduarda': 60, 
+                       'Academia da Berlinda': 30, 'Martins': 25, 'Igor de Carvalho': 25}
+fortuna_dos_artistas = {'Priscila Senna': 10000, 'Eduarda': 9990, 'Academia da Berlinda': 9995,
+                        'Martins': 9970, 'Igor de Carvalho': 9965}
+
+def lucro_apos_a_tributacao(qtd_discos, valor_arrecadado):
+    if qtd_discos == 1:
+        return valor_arrecadado
+    elif qtd_discos > 1 and qtd_discos <= 3:
+       return valor_arrecadado*0.98
+    elif qtd_discos > 3 and qtd_discos <= 5:
+        return valor_arrecadado *0.95
+    else:
+        return valor_arrecadado*0.93
+  
+
+numero_de_artistas = int(input('Digite o número de artistas selecionados: '))
+
+for i in range(numero_de_artistas):
+    artista_selecionado, qtd_discos = input().split(' - ')
+    qtd_discos = int(qtd_discos)
+
+    if artista_selecionado in discos_por_artistas.keys():
+        
+        discos_por_artistas[artista_selecionado] += qtd_discos
+        fortuna_dos_artistas[artista_selecionado] += lucro_apos_a_tributacao(qtd_discos, qtd_discos*20) #cada disco custa R$ 20,00
+
+
+print(discos_por_artistas)
+print(fortuna_dos_artistas)
